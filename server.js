645f677a45b08cca57cca5c90c6dd654dc9d270c
Hafiz Mughees <mughees605@gmail.com>
2017-02-13 20:48:59 +0500

    adding schema and post and get request

--- server.js
+++ server.js
@@ -1,17 +1,57 @@
 var express = require("express");
 var mongoose = require("mongoose");
-
-
-
-
+var bodyParser = require("body-parser");
 
 
 var app = express();
-
 var port = (process.env.PORT || 3000);
+
+app.use(bodyParser.json());
+
+var studentSchema = new mongoose.Schema({
+    name : String,
+    rollNum:Number,
+    class:String
+})
+
+var studentModel = mongoose.model("student",studentSchema);
+
+app.post("/add",function(req,res,next){
+
+    var newStudent = new studentModel({
+        name : req.body.name,
+        class:req.body.class,
+        rollNum : req.body.rollNum
+    })
+    newStudent.save(function(err,data){
+
+        if(!err){
+            console.log("student is saved");
+            res.send("student is saved");
+        }
+        else{
+            res.send("student saving failed");
+            console.log("student savig failed");
+        }
+
+    })
+})
+
+
 app.get("/",function(req,res,next){
 
-    res.send("hello world");
+   // res.send("hello world")
+   studentModel.find(function(err,doc){
+   if(err){
+       res.send(err);
+   }
+   else{
+       res.send(doc);
+   }
+   })
+       
+    
+
 
 
 })
